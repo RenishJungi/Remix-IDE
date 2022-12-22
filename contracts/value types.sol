6b7257666923834a2aa295b25f9862a48b5dc82b
RenishJungi <renishj2011@gmail.com>
2022-12-22 12:12:24 -0800

    Remix test

---
+++ contracts/value types.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract valuetypes {
+
+bool public boolTemp = true;
+
+uint public temp = 243;
+
+int public temp2 = 14;
+
+address public addr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+  bytes32 public b3;
+
+
+
+}
